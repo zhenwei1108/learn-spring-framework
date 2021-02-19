4afeb54a11fb926deaac3891ed4fb4836cd7a3d3
zhenwei1108 <zhangzhenwei@bjca.org.cn>
2021-02-19 18:12:43 +0800

    补充ClassLoader学习

--- spring-demo/src/main/java/com/zhenwei/spring/demo/loader/ClassLoaderDemo.java
+++ spring-demo/src/main/java/com/zhenwei/spring/demo/loader/ClassLoaderDemo.java
@@ -1,9 +1,14 @@
 package com.zhenwei.spring.demo.loader;
 
-public class ClassLoaderDemo {
+public class ClassLoaderDemo extends ClassLoader{
 
+  @Override
+  public Class<?> loadClass(String name) throws ClassNotFoundException {
+    return super.loadClass(name);
+  }
 
-
-
-
+  @Override
+  protected Class<?> findClass(String name) throws ClassNotFoundException {
+    return super.findClass(name);
+  }
 }
